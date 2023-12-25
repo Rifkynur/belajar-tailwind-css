44c13499139f854dd4294ab1bb5b056603c768d6
Rifkynur <ythrifky619@gmail.com>
2023-12-25 12:16:46 +0700

    add darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,15 @@
-// navbar fixed
+// navbar fixed dan tombol to top
 window.onscroll = function () {
   const header = document.querySelector("header");
+  const top = document.querySelector("#top");
   if (window.scrollY > 0) {
     header.classList.add("navbar-fixed");
+    top.classList.remove("hidden");
+    top.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    top.classList.add("hidden");
+    top.classList.remove("flex");
   }
 };
 // hamburger button
@@ -23,3 +28,22 @@ window.addEventListener("click", function (e) {
   }
   console.log(e.target);
 });
+
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+// pindahkan toggel sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
